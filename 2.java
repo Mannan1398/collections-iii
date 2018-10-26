614cd1dc9f0231033297a7051d2e3e4e0ba7492c
Mannan1398 <42761505+Mannan1398@users.noreply.github.com>
2018-10-26 12:42:01 +0530

    Create 2.java

---
+++ 2.java
@@ -0,0 +1,23 @@
+import java.util.*;
+ class q2
+ {
+
+	public static void main(String[] args) 
+     {
+     ArrayList<String> s=new ArrayList<String>();
+     s.add("Gaurav sharma");
+     s.add("Tamanna Sharma");
+     s.add("Ajayveer");
+     Collections.sort(s);
+     System.out.println(s);
+
+     ArrayList<Integer> i=new ArrayList<Integer>();
+
+     i.add(1);
+     i.add(20);
+     i.add(3);
+     Collections.sort(i);
+     System.out.println(i);
+	}
+
+}
